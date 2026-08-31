de555d41893bb56193c1374269a5ea81e181e685
itmanbanana <ben@benstack.dev>
2026-08-31 18:51:26 -0400

    web: sort file and dir entries by name in view

--- apps/web/src/components/drive/drive-content.tsx
+++ apps/web/src/components/drive/drive-content.tsx
@@ -7,22 +7,29 @@ import { DriveDirectory } from "./drive-directory";
 import { DriveFile } from "./drive-file";
 import { Separator } from "../ui/separator";
 
+type DirType = z.infer<typeof driveSchema.driveDir>;
+type FileType = z.infer<typeof driveSchema.driveFile>;
+
+const compareDirs = (dir1: DirType, dir2: DirType): number => (dir1.name < dir2.name) ? -1 : 1;
+
+const compareFiles = (file1: FileType, file2: FileType): number => (file1.name < file2.name) ? -1 : 1;
+
 export function DriveContent() {
-  const [dirs, setDirs] = useState<z.infer<typeof driveSchema.driveDir>[] | null>(null);
-  const [files, setFiles] = useState<z.infer<typeof driveSchema.driveFile>[] | null>(null);
+  const [dirs, setDirs] = useState<DirType[] | null>(null);
+  const [files, setFiles] = useState<FileType[] | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const rootDirId = await driveClient.getRootId();
       if (rootDirId) {
         const rawData = await driveClient.getDirContent(rootDirId);
-        setDirs(rawData.subDirs);
-        setFiles(rawData.files);
+        setDirs(rawData.subDirs.sort(compareDirs));
+        setFiles(rawData.files.sort(compareFiles));
       }
     };
 
     if (!dirs && !files) fetchData();
-  }, []);
+  }, [setDirs, setFiles, dirs, files]);
 
   return (
     <>
